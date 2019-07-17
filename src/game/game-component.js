11f5747d05c1b1d6845c268c23e76a1167dca769
Jan Hesters <jan.hesters@rwth-aachen.de>
2019-07-17 16:58:23 +0200

    Style game container.

--- src/game/game-component.js
+++ src/game/game-component.js
@@ -1,16 +1,29 @@
 import Container from '@material-ui/core/Container';
+import Paper from '@material-ui/core/Paper';
+import { makeStyles } from '@material-ui/core/styles';
 import React from 'react';
 
 import History from '../history';
 import QuestionForm from '../question-form';
 import Scores from '../scores';
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    padding: theme.spacing(3, 2),
+    height: '100%',
+  },
+}));
+
 function Game() {
+  const classes = useStyles();
+
   return (
     <Container maxWidth="md" className="game-container">
-      <Scores />
-      <QuestionForm />
-      <History />
+      <Paper className={classes.root}>
+        <Scores />
+        <QuestionForm />
+        <History />
+      </Paper>
     </Container>
   );
 }
