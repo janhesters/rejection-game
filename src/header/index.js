ee4d457f9c45f404778bd1c08c29a76cf32211a0
Jan Hesters <jan.hesters@rwth-aachen.de>
2019-07-20 21:22:26 +0200

    Fix logout logic.

--- src/header/index.js
+++ src/header/index.js
@@ -1,15 +1,27 @@
 import { Auth } from 'aws-amplify';
 import { connect } from 'react-redux';
 
-import { getUser } from '../auth/auth-reducer';
+import { clearUser, getUser } from '../auth/auth-reducer';
 import Header from './header-component';
 import strings from './strings';
 
-// NOTE: Is this okay?
 const mapStateToProps = state => ({
   appTitle: strings.appTitle,
-  onClick: () => Auth.signOut(),
   user: getUser(state),
 });
 
-export default connect(mapStateToProps)(Header);
+const mergeProps = (stateProps, dispatchProps, ownProps) => ({
+  ...stateProps,
+  ...ownProps,
+  ...dispatchProps,
+  onClick: async () => {
+    await Auth.signOut();
+    dispatchProps.clearUser();
+  },
+});
+
+export default connect(
+  mapStateToProps,
+  { clearUser },
+  mergeProps
+)(Header);
